0e30d53d967c8850ca1feccfcb3e52b21d2d0c44
VasliddinAli <vasliddinali@gmail.com>
2023-05-11 11:31:24 +0500

    ...

--- resources/views/backend/ceo/ceo_view.blade.php
+++ resources/views/backend/ceo/ceo_view.blade.php
@@ -1,144 +1,145 @@
 @extends('admin.admin_master')
 
 @section('admin')
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1>CEO</h1>
-                </div>
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Asosiy</a></li>
-                        <li class="breadcrumb-item active">CEO</li>
-                    </ol>
-                </div>
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1>CEO</h1>
             </div>
-        </div><!-- /.container-fluid -->
-    </section>
-    <!-- Main content -->
-    <section class="content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-8">
-                    <div class="card">
-                        <!-- <div class="card-header">
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Asosiy</a></li>
+                    <li class="breadcrumb-item active">CEO</li>
+                </ol>
+            </div>
+        </div>
+    </div><!-- /.container-fluid -->
+</section>
+<!-- Main content -->
+<section class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-8">
+                <div class="card">
+                    <!-- <div class="card-header">
                             <h3 class="card-title">Categories</h3>
                         </div> -->
-                        <!-- /.card-header -->
-                        <div class="card-body">
-                            <table id="example1" class="table table-bordered table-striped">
-                                <thead>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <table id="example1" class="table table-bordered table-striped">
+                            <thead>
                                 <tr>
                                     <th>ID</th>
                                     <th>Nomi</th>
                                     <th>Telefon</th>
+                                    <th>Bot Chat ID</th>
                                     <th>Boshqarish</th>
                                 </tr>
-                                </thead>
-                                <tbody>
+                            </thead>
+                            <tbody>
                                 @foreach ($ceos as $item)
-                                    <tr>
-                                        <td width="1%">{{ $item->id }}</td>
-                                        <td>{{ $item->name }}</td>
-                                        <td>{{ $item->phone }}</td>
-                                        <td width="12%">
-                                            <a href="{{ route('ceo.show', $item->id) }}" class="btn btn-primary" title="Ko'rish"><i class="fas fa-eye"></i></a>
-                                            <a href="{{ route('ceo.edit', $item->id) }}" class="btn btn-info" title="O'zgartirish"><i class="fas fa-edit"></i></a>
-                                            <a href="{{ route('ceo.delete', $item->id) }}" class="btn btn-danger" title="O'chirish" id="delete"><i class="fas fa-trash"></i></a>
-                                        </td>
-                                    </tr>
+                                <tr>
+                                    <td width="1%">{{ $item->id }}</td>
+                                    <td>{{ $item->name }}</td>
+                                    <td>{{ $item->phone }}</td>
+                                    <td>{{ $item->bot_id == null ? "NULL" : $item->bot_id }}</td>
+                                    <td width="12%">
+                                        <a href="{{ route('ceo.show', $item->id) }}" class="btn btn-primary" title="Ko'rish"><i class="fas fa-eye"></i></a>
+                                        <a href="{{ route('ceo.edit', $item->id) }}" class="btn btn-info" title="O'zgartirish"><i class="fas fa-edit"></i></a>
+                                        <a href="{{ route('ceo.delete', $item->id) }}" class="btn btn-danger" title="O'chirish" id="delete"><i class="fas fa-trash"></i></a>
+                                    </td>
+                                </tr>
                                 @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-                        <!-- /.card-body -->
+                            </tbody>
+                        </table>
                     </div>
-                    <!-- /.card -->
+                    <!-- /.card-body -->
                 </div>
-                <div class="col-md-4">
-                    <!-- general form elements -->
-                    <div class="card">
-                        <div class="card-header">
-                            <h3 class="card-title">Brend qo'shish</h3>
-                        </div>
-                        <!-- /.card-header -->
-                        <!-- form start -->
-                        <form method="POST" action="{{ route('ceo.store') }}" enctype="multipart/form-data">
-                            @csrf
-                            <div class="card-body">
-                                <div class="col-md-12">
-                                    <div class="row">
-                                        <div class="col-md-12">
-                                            <div class="form-group">
-                                                <label>Nomi</label>
-                                                <input type="text" name="name" class="form-control">
-                                                @error('name')
-                                                <span class="text-danger">{{ $message }}</span>
-                                                @enderror
-                                            </div>
-                                            <div class="form-group">
-                                                <label>Telefon</label>
-                                                <input type="number" name="phone" class="form-control">
-                                                @error('phone')
-                                                <span class="text-danger">{{ $message }}</span>
-                                                @enderror
-                                            </div>
-                                            <div class="form-group">
-                                                <label>Parol</label>
-                                                <input type="text" name="password" class="form-control">
-                                                @error('password')
-                                                <span class="text-danger">{{ $message }}</span>
-                                                @enderror
-                                            </div>
+                <!-- /.card -->
+            </div>
+            <div class="col-md-4">
+                <!-- general form elements -->
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Brend qo'shish</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <!-- form start -->
+                    <form method="POST" action="{{ route('ceo.store') }}" enctype="multipart/form-data">
+                        @csrf
+                        <div class="card-body">
+                            <div class="col-md-12">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label>Nomi</label>
+                                            <input type="text" name="name" class="form-control">
+                                            @error('name')
+                                            <span class="text-danger">{{ $message }}</span>
+                                            @enderror
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Telefon</label>
+                                            <input type="number" name="phone" class="form-control">
+                                            @error('phone')
+                                            <span class="text-danger">{{ $message }}</span>
+                                            @enderror
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Parol</label>
+                                            <input type="text" name="password" class="form-control">
+                                            @error('password')
+                                            <span class="text-danger">{{ $message }}</span>
+                                            @enderror
                                         </div>
                                     </div>
-                                    <div class="d-flex justify-content-end">
-                                        <input type="submit" class="btn btn-rounded btn-primary" value="Qo'shish">
-                                    </div>
+                                </div>
+                                <div class="d-flex justify-content-end">
+                                    <input type="submit" class="btn btn-rounded btn-primary" value="Qo'shish">
                                 </div>
                             </div>
-                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card-body -->
 
-                        </form>
-                    </div>
-                    <!-- /.card -->
+                    </form>
                 </div>
+                <!-- /.card -->
             </div>
-            <!-- /.row -->
-
         </div>
-        <!-- /.container-fluid -->
-    </section>
-    <!-- /.content -->
+        <!-- /.row -->
+
+    </div>
+    <!-- /.container-fluid -->
+</section>
+<!-- /.content -->
 
 
 
-    <script type="text/javascript">
-        $(document).ready(function () {
-            $('a[name="center_id"]').on('change', function () {
-                var center_id = $(this).val();
-                if (center_id) {
-                    $.ajax({
-                        url: "{{  url('/training/center/image/ajax') }}/" + center_id,
-                        type: "GET",
-                        dataType: "json",
-                        // success: function (data) {
-                        //     var d = $('select[name="district_id"]').empty();
-                        //     $.each(data, function (key, value) {
-                        //         $('select[name="district_id"]').append('<option value="' + value.id + '">' + value.name_uz + '</option>');
-                        //     });
-                        // },
-                    });
-                } else {
-                    alert('danger');
-                }
-            });
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('a[name="center_id"]').on('change', function() {
+            var center_id = $(this).val();
+            if (center_id) {
+                $.ajax({
+                    url: "{{  url('/training/center/image/ajax') }}/" + center_id,
+                    type: "GET",
+                    dataType: "json",
+                    // success: function (data) {
+                    //     var d = $('select[name="district_id"]').empty();
+                    //     $.each(data, function (key, value) {
+                    //         $('select[name="district_id"]').append('<option value="' + value.id + '">' + value.name_uz + '</option>');
+                    //     });
+                    // },
+                });
+            } else {
+                alert('danger');
+            }
         });
-    </script>
+    });
+</script>
 
 @endsection
-
