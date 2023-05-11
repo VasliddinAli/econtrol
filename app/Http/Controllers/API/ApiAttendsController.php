a6827491adbd719128a76e9600d0dd9edb923ccc
VasliddinAli <vasliddinali@gmail.com>
2023-05-11 18:16:43 +0500

    ...

--- app/Http/Controllers/API/ApiAttendsController.php
+++ app/Http/Controllers/API/ApiAttendsController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\Attendance;
+use App\Models\Employee;
+use App\Models\Purpose;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ApiAttendsController extends Controller
@@ -14,8 +17,31 @@ class ApiAttendsController extends Controller
         return $this->sendResponse($attendances, true, "");
     }
 
+
+
+    public function checkDate()
+    {
+        $time = Carbon::now();
+        $now_time = Carbon::now()->timestamp;
+        $check_time = Carbon::create($time->year, $time->month, $time->day, 8, 0, 0)->timestamp;
+        if ($now_time > $check_time) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
     public function addAttends(Request $request)
     {
+        $moment = true;
+        $date = $this->checkdate();
+        $purposes = [];
+
+        if (!$date && $request->type == 'input') {
+            $moment = false;
+            $purposes = Purpose::select('id', 'purpose')->get();
+        }
+        $employee = Employee::where('id', $request->employee_id)->select('id', 'name', 'position')->first();
         $file = $request->file('image');
         $imageName = time() . '.' . $file->extension();
         $imagePath = public_path() . '/upload/images';
@@ -25,11 +51,19 @@ class ApiAttendsController extends Controller
         $attendance = new Attendance();
         $attendance->type = $request->type;
         $attendance->image = $image;
-        $attendance->date = $request->date;
+        $attendance->date = Carbon::now();
         $attendance->employee_id = $request->employee_id;
         $attendance->device_id = $request->device_id;
         $attendance->save();
-        return $this->sendResponse(null, true, "");
+
+        $response = [
+            'employee' => $employee,
+            'now_time' => Carbon::now()->format('H:i'),
+            'moment' => $moment,
+            'purposes' => $purposes,
+            'attendance_id' => $attendance->id,
+        ];
+        return $this->sendResponse($response, true, "");
     }
 
     public function forLatePurpose(Request $request)
