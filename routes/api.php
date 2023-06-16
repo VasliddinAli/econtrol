a45e88f44399dcd05a1d005fbbca0e0f1105f119
VasliddinAli <vasliddinali@gmail.com>
2023-06-16 18:24:15 +0500

    ...

--- routes/api.php
+++ routes/api.php
@@ -23,28 +23,43 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::prefix('ceo')->group(function () {
+    Route::get('list', [ApiCEOController::class, 'getCeos']);
+    Route::post('add', [ApiCEOController::class, 'addCeo']);
+    Route::post('update/{id}', [ApiCEOController::class, 'updateCeo']);
+    Route::get('delete/{id}', [ApiCEOController::class, 'deleteCeo']);
+    Route::post('login', [ApiCEOController::class, 'ceoLogin']);
+});
+
 Route::prefix('device')->group(function () {
     Route::get('list', [ApiDeviceController::class, 'getDevices']);
-    // Route::post('add', [ApiDeviceController::class, 'addDevice']);
-    // Route::get('{id}/detail', [ApiDeviceController::class, 'getDevice']);
-    // Route::post('{id}/update', [ApiDeviceController::class, 'updateDevice']);
-    // Route::get('{id}/delete', [ApiDeviceController::class, 'deleteDevice']);
+    Route::post('add', [ApiDeviceController::class, 'addDevice']);
+    Route::get('detail/{id}', [ApiDeviceController::class, 'getDevice']);
+    Route::post('update/{id}', [ApiDeviceController::class, 'updateDevice']);
+    Route::get('delete/{id}', [ApiDeviceController::class, 'deleteDevice']);
     Route::post('find', [ApiDeviceController::class, 'findDeviceByPassword']);
 });
+
 Route::prefix('employee')->group(function () {
     Route::get('list', [ApiEmployeeController::class, 'getEmployees']);
+    Route::get('warn/list/{id}', [ApiEmployeeController::class, 'getWarningEmployees']);
+    Route::get('view/{id}', [ApiEmployeeController::class, 'getEmployee']);
     Route::post('add', [ApiEmployeeController::class, 'addEmployee']);
-    Route::get('detail/{id}', [ApiEmployeeController::class, 'getEmployee']);
+    Route::post('update/pin/{id}', [ApiEmployeeController::class, 'updatePin']);
     Route::post('update/{id}', [ApiEmployeeController::class, 'updateEmployee']);
     Route::get('delete/{id}', [ApiEmployeeController::class, 'deleteEmployee']);
     Route::get('{pinCode}/find', [ApiEmployeeController::class, 'findEmployeeByPinCode']);
 });
-Route::prefix('attendance')->group(function () {
-    Route::get('list', [ApiAttendsController::class, 'getAttendance']);
-    Route::post('add', [ApiAttendsController::class, 'addAttends']);
-    Route::post('add/offline', [ApiAttendsController::class, 'addAttendsOffline']);
-    Route::post('late/purpose', [ApiAttendsController::class, 'forLatePurpose']);
-});
+
 Route::prefix('purpose')->group(function () {
-    Route::get('list', [ApiPurposeController::class, 'getPurpose']);
+    Route::get('list', [ApiPurposeController::class, 'purposeView']);
+});
+
+Route::prefix('attendance')->group(function () {
+    Route::get('list', [ApiAttendanceController::class, 'getAttendance']);
+    Route::post('add', [ApiAttendanceController::class, 'addAttends']);
+    Route::post('add/offline', [ApiAttendanceController::class, 'addAttendsOffline']);
+    Route::post('late/purpose', [ApiAttendanceController::class, 'forLatePurpose']);
+    Route::get('delete/{id}', [ApiAttendanceController::class, 'AttendanceDelete']);
+    Route::post('warning/{id}', [ApiAttendanceController::class, 'AttendanceWarning']);
 });
